bae1702e28d0af822ad30d04a30cb09a73759e74
michelleshelly <michelle.minjiegoh@gmail.com>
2021-03-06 12:59:44 +0800

    end of lesson6

---
+++ Text/Lesson6/list.py
@@ -0,0 +1,11 @@
+#using range 
+range(a,b)
+# means [a,a+1, a+2, ... b-1]
+range(b)
+# means range (0,b)
+#overload = same function name, but diff function
+
+#for argument go down the lists, answer cant be true
+#meaning cannot put "joseph" in ["joseph" , "glenn" , "sally"] -> answer is true, gives error
+#while means going through the list, loop
+#signature defined by name and function
